74f5a0c353e04b008992c19b4c2c4472504b8fdb
Pankaj <pankkaj64@gmail.com>
2026-06-29 12:45:05 +0400

    Feat: countnumber

--- countvowels/countvowels.py
+++ countvowels/countvowels.py
@@ -1,11 +1,12 @@
 def countvowels(string):
-    vowels = "aeiou"
+    vowels = 'aeiou'
     count = 0
     for char in string:
-        if char.lower() in vowels:   # .lower() so "A" counts too
+        if char in vowels:
             count += 1
-    return count                     # <-- return the number, don't just print
-
+    return count
 
 if __name__ == "__main__":
-    print(countvowels("hello"))      # 2
+    print(countvowels("hello"))
+
+
