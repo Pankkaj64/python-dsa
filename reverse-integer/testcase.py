67bbf9243198e01888d2e2302d1b5435d78d947e
Pankaj <pankkaj64@gmail.com>
2026-07-21 17:19:40 +0400

    Reverse Integer

---
+++ reverse-integer/testcase.py
@@ -0,0 +1,10 @@
+from reverse import reverse
+
+
+print(reverse(123))        # 321
+print(reverse(-123))       # -321
+print(reverse(120))        # 21
+print(reverse(0))          # 0
+print(reverse(1534236469)) # 0 (overflow)
+print(reverse(-1200))      # -21
+print(reverse(100))        # 1
