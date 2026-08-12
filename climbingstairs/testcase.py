3e070b451d827c2aa3a47ea38460ebbe17a245bd
Pankaj <pankkaj64@gmail.com>
2026-08-12 15:40:38 +0400

    Climbing Stairs

---
+++ climbingstairs/testcase.py
@@ -0,0 +1,28 @@
+import unittest
+
+from climbing_stairs import climb_stairs
+
+
+class TestClimbingStairs(unittest.TestCase):
+
+    def test_one_stair(self):
+        self.assertEqual(climb_stairs(1), 1)
+
+    def test_two_stairs(self):
+        self.assertEqual(climb_stairs(2), 2)
+
+    def test_three_stairs(self):
+        self.assertEqual(climb_stairs(3), 3)
+
+    def test_four_stairs(self):
+        self.assertEqual(climb_stairs(4), 5)
+
+    def test_five_stairs(self):
+        self.assertEqual(climb_stairs(5), 8)
+
+    def test_ten_stairs(self):
+        self.assertEqual(climb_stairs(10), 89)
+
+
+if __name__ == "__main__":
+    unittest.main()
