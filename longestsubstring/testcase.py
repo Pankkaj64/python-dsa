cd5b5646e4f733fbc3d70038ef00d0ee6e0cbdfa
Pankaj <pankkaj64@gmail.com>
2026-09-01 12:11:50 +0400

    Length of substring

---
+++ longestsubstring/testcase.py
@@ -0,0 +1,7 @@
+from main import  lengthOfLongestSubstring
+
+print(lengthOfLongestSubstring("abcabcbb"))  # 3
+print(lengthOfLongestSubstring("bbbbb"))     # 1
+print(lengthOfLongestSubstring("pwwkew"))    # 3
+print(lengthOfLongestSubstring(""))          # 0
+print(lengthOfLongestSubstring("abcdef"))    # 6
